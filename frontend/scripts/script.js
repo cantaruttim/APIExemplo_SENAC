6581fc79e28dc56859eb0c3c2bbbc4cd20a686e2
cantaruttim <cantaruttim@outlook.com>
2025-11-10 22:10:16 -0300

    methodo get

--- frontend/scripts/script.js
+++ frontend/scripts/script.js
@@ -1,1 +1,26 @@
-console.log("Servidor rodando! Vai!")
+const apiURL = 'http:localhost:5034/api/gastos';
+
+const getGastosList = async () => {
+    try {
+        // que vamos implementar o método GET
+        const response = await fetch(
+            apiURL,
+            {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }
+        );
+
+        if (!response.ok) {
+            throw new Error('Erro enquanto buscava lista de gastos');
+        }
+
+        const gastos = await response.json();
+        console.log(gastos);
+
+    } catch (error) {
+        console.error(error.message);
+    }
+}
